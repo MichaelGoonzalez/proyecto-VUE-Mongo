0870c50aeae569893c8c32db9459ecdcd7c821c9
Alejandro Galvis Florez <alejito20401@hotmail.com>
2021-10-04 15:41:54 -0500

    Revert "Merge branch 'Alejandro' of https://github.com/MichaelGoonzalez/proyecto-VUE-Mongo into Alejandro"
    This reverts commit e61157b77962c59481b89b13d55d3794881c6972, reversing
    changes made to a9dd36b1c24d12b974570a1a423556b50daa4f3b.

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,13 @@
                 <div class="">
                     {{obtenerAño()}}
                     &copy;  Carvajal Materiales | Todos los derechos reservados.
-                    <div class="mx-4">
-                        <a class="text-white" href="https://www.facebook.com/Carvajalmateriales" target="_blank"><i class="fab fa-facebook"></i></a>
-                        <a class="text-white" href="https://www.instagram.com/carvajalmateriales/" target="_blank"><i class="fab fa-instagram"></i></a>
-                        <a class="text-white" href="https://api.whatsapp.com/send?phone=%2B573003659340&fbclid=IwAR1Fs2TmR-vYJG0c8c1zhm4XzCFEJjWr2zbsaMjivUyG2SCrSoFVYx49qwA" target="_blank"><i class="fab fa-whatsapp"></i></a>
-                    </div>
+                </div>
+                <div class="mx-4">
+                    <a class="text-white" href="https://www.facebook.com/Carvajalmateriales" target="_blank"><i class="fab fa-facebook"></i></a>
+                    <a class="text-white" href="https://www.instagram.com/carvajalmateriales/" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a class="text-white" href="https://api.whatsapp.com/send?phone=%2B573003659340&fbclid=IwAR1Fs2TmR-vYJG0c8c1zhm4XzCFEJjWr2zbsaMjivUyG2SCrSoFVYx49qwA" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                </div>
+                <div class="">
                     <a class="text-white text-decoration-none" href="login.vue">Administrador</a>
                 </div>
             </div>
