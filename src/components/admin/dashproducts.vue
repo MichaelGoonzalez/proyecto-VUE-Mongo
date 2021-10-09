0985ca173034eb1a78c72a6ff09a0e8f10eafc96
Alejandro Galvis Florez <alejito20401@hotmail.com>
2021-10-09 17:05:44 -0500

    MongoDB - iniciar sesion / productos

--- src/components/admin/dashproducts.vue
+++ src/components/admin/dashproducts.vue
@@ -4,10 +4,15 @@
             <div class="row">
                 <div class="barra-lateral col-md-2 col-sm-auto">
                     <nav class="menu d-flex d-sm-block justify-content-center align-items-center flex-wrap" >
-                        
-                        <a href="#"><i class="fas fa-user-friends"></i><span>Usuarios</span></a>
-                        <a href="#"><i class="fas fa-store"></i><span>Productos</span></a>
-                        <a href="#"><i class="fas fa-clipboard-list"></i><span>Categorias</span></a>
+                        <router-link style="text-decoration:none" to="/dashusers">
+                            <i class="fas fa-user-friends"></i><span>Usuarios</span>
+                        </router-link>
+                        <router-link style="text-decoration:none" to="/dashproducts">
+                            <i  class="fas fa-store"></i><span>Productos</span>
+                        </router-link>
+                        <!-- <router-link to="">
+                            <i class="fas fa-clipboard-list"></i><span>Categorias</span>
+                        </router-link> -->
                     </nav>
                 </div>
 
@@ -152,3 +157,9 @@ export default {
     }
 }
 </script>
+
+<style>    
+    #nav a.router-link-exact-active {
+        color: #f6821f;
+    }
+</style>
