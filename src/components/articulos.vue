17c97bb4db1a4ee8470aecdda5ebbc6ad7542eca
jhonf1992 <87623618+jhonf1992@users.noreply.github.com>
2021-10-10 11:39:10 -0500

    fix
    fix

--- src/components/articulos.vue
+++ src/components/articulos.vue
@@ -37,7 +37,7 @@
                                     <h6 class="card-title color-marca">{{item.clase}}</h6>                                    
                                     <h5 class="card-title text-black">{{item.titulo}}</h5>
                                     <p class="card-text text-black">
-                                        {{item.descripcion}}<a href="#" class="text-danger">Leer más...</a>
+                                        {{item.descripcion}}<a :href = "item.url" class="text-danger">  Leer más...</a>
                                     </p>
                                 </div>
                             </div>
@@ -54,54 +54,51 @@
         return {
             articulosArray: [
                 {
+                    url:"https://www.portafolio.co/innovacion/ferreteros-podran-vender-soat-gracias-con-tuel-553253",
                     img: require('../assets/noticiatul.jpg'),
                     clase: "Noticia",
                     titulo: "Más de 5.000 ferreteros podrán vender Soat usando app TÜL",
                     descripcion: "La plataforma llegó a un acuerdo con AXA Colpatria para que se pudiera prestar ese servicio. Cada año, los propietarios de vehículos automotores en Colombia deben renovar su Seguro Obligatorio de Accidentes de Tránsito (Soat)." 
-                    +"  " + "Pensando en cómo seguir facilitando la adquisición de este seguro, la aplicación colombiana TÜL realizó una alianza con AXA Colpatria para que más de 5.000 ferreteros del país puedan vender el Soat digital. De esta manera, además de ofrecer materiales de construcción y herramientas, también incluirán un servicio nuevo e innovador que nunca se había comercializado por este canal."+" "+ "La alianza entre TÜL y AXA Colpatria, a través de su intermediario de seguros Orangreen Insurance, permitirá a miles de colombianos en las principales ciudades de Colombia adquirir su Soat digital de forma ágil en la ferretería más cercana."+"  "+" Según Andrés Ramírez, gerente de Asuntos Públicos de TÜL, este es un primer gran paso que da la empresa para lograr aumentar y diversificar las ventas de los ferreteros utilizando las ventajas que trae la tecnología." + " " + "Desde de TÜL también anunciaron que, en los próximos meses, el ferretero que tenga la app descargada podrá vender recarga de minutos de celular, alquilar herramientas y andamios y hacer pagos de facturas de servicios públicos. Hay que recordar que TÜL es una 'startup' que nació hace un año con el apoyo de Rappi y su objetivo es digitalizar el mercado de la ferretería."
+                    +"  " + "Pensando en cómo seguir facilitando la adquisición de este seguro, la aplicación colombiana TÜL realizó una alianza con AXA Colpatria para que más de 5.000 ferreteros del país puedan vender el Soat digital. De esta manera, además de ofrecer materiales de construcción y herramientas, también incluirán un servicio nuevo e innovador que nunca se había comercializado por este canal."
+
+                },
+                {
+                    url:"https://www.portafolio.co/economia/infraestructura/plantas-termicas-listas-para-respaldar-el-sistema-electrico-557188",
+                    img: require('../assets/termovalle.jpg'),
+                    clase: "Noticias",
+                    titulo: "Plantas térmicas, listas para respaldar el sistema eléctrico",
+                    descripcion: "De los proyectos a corto y mediano plazo, sobresalen las expansiones de Termovalle y Termoyopal." + " "+" Con la misión de servir como respaldo al sistema eléctrico del país y así garantizar la energía en firme, se está poniendo a tono un parque térmico con más de 40 plantas.De acuerdo al registro de la Upme, a las 34 termoeléctricas que ya operan se sumarían 11 centrales cuya entrada en operación está programada hasta finales de 2022. Si el articulo te resulta interesante tienes toda la informacion a continuación." 
                     
 
                 },
                 {
-                    img: require('../assets/tutorial.png'),
-                    clase: "Tutorial",
-                    titulo: "¿Cómo ajustar los muebles adecuadamente?",
-                    descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae laborum qui vero dignissimos" 
-                    +"provident, fuga cum illum eos voluptate rerum, est ipsam eum nesciunt? Sequi explicabo ipsam illum"
-                    +"laborum culpa!"
+                    img: require('../assets/ferretero-01.jpg'),
+                    clase: "Noticias",
+                    titulo: "El Gran Salón Ferretero contará con 50 expositores y espera reactivar esta industria",
+                    descripcion: "El evento tendrá una afluencia aproximada de 2.000 personas, apenas por debajo de la cifra presentada en la feria de 2019. Durante el 8 y 9 de octubre se llevará a cabo El Gran Salón ferretero, de forma presencial. Este evento de negocios pretende facilitar un espacio de encuentro entre marcas, distribuidores y profesionales ferreteros." + "" + "Este se llevará a cabo en el centro comercial Mall Plaza NQS de Bogotá, si deseas mas informacion te invitamos a seguir leyendo... "
+                    
 
                 },
                 {
-                    img: require('../assets/tutorial.png'),
-                    clase: "Tutorial",
-                    titulo: "¿Cómo ajustar los muebles adecuadamente?",
-                    descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae laborum qui vero dignissimos" 
-                    +"provident, fuga cum illum eos voluptate rerum, est ipsam eum nesciunt? Sequi explicabo ipsam illum"
-                    +"laborum culpa!"
+                    url:"https://www.portafolio.co/economia/consejos-para-ahorrar-y-reducir-el-consumo-de-energia-en-casa-557158",
+                    img: require('../assets/ahorro_energia.jpg'),
+                    clase: "Consejos",
+                    titulo: "Consejos para ahorrar y reducir el consumo de energía en casa",
+                    descripcion: "A pesar de que muchos sectores han abierto y la nueva normalidad se abre paso, lo cierto es que el teletrabajo se consolidó durante los meses duros de pandemia. Así las cosas, algunas empresas han optado por modelos de trabajo híbridos, lo que permite a las personas estar mas tiempo en sus hogares.Por este motivo, Enel-Condensa ofrece unas recomendaciones para reducir el costo en sus facturas.Electrodomésticos. Prefiera electrodomésticos con desempeño energético eficiente.Aparatos tecnológicos. Mantenga cargando su celular, tableta o dispositivos lo estrictamente necesario y desconecte el cargador apenas termine de completarse la batería."
 
                 },
                 {
-                    img: require('../assets/tutorial.png'),
+                    
+                    img: require('../assets/mesa.jpg'),
                     clase: "Tutorial",
-                    titulo: "¿Cómo ajustar los muebles adecuadamente?",
-                    descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae laborum qui vero dignissimos" 
-                    +"provident, fuga cum illum eos voluptate rerum, est ipsam eum nesciunt? Sequi explicabo ipsam illum"
-                    +"laborum culpa!"
-
+                    titulo: "¿Cómo hacer una mesa auxiliar?",
+                    descripcion: "Las mesas auxiliares son indispensables en el hogar porque resultan muy prácticas y aportan estilo. ¡Aprende a hacer una tú mismo!" + "                 "+ " Hacer una mesa auxiliar es una gran idea. Se trata de un mueble funcional y aporta estilo a la decoración. Este tipo de mesas complementan muy bien diversos espacios; permiten reposar objetos como floreros, libros o cualquier otro tipo de elemento decorativo." 
+                    
                 },
                 {
                     img: require('../assets/tutorial.png'),
-                    clase: "Tutorial",
-                    titulo: "¿Cómo ajustar los muebles adecuadamente?",
-                    descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae laborum qui vero dignissimos" 
-                    +"provident, fuga cum illum eos voluptate rerum, est ipsam eum nesciunt? Sequi explicabo ipsam illum"
-                    +"laborum culpa!"
-
-                },
-                {
-                    img: require('../assets/tutorial.png'),
-                    clase: "Tutorial",
-                    titulo: "¿Cómo ajustar los muebles adecuadamente?",
+                    clase: "Consejos",
+                    titulo: "5 consejos útiles si deseas poner tu casa en alquiler",
                     descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae laborum qui vero dignissimos" 
                     +"provident, fuga cum illum eos voluptate rerum, est ipsam eum nesciunt? Sequi explicabo ipsam illum"
                     +"laborum culpa!"
