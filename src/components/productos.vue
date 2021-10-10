a819d0ff9dde2d297edfc6419530e8cdf880782d
Alejandro Galvis Florez <alejito20401@hotmail.com>
2021-10-10 10:31:13 -0500

    Verificar si el usuario está ingresado

--- src/components/productos.vue
+++ src/components/productos.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="container">
+        <div id= "contenedor" class="container">
             <div class="row">
                 <section class="col-md-10 mx-auto">
                     <ul class="nav nav-tabs nav-tabsp nav-fill" id="productos" role="tablist">
@@ -9,8 +9,7 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" id="materiales-tab" data-bs-toggle="tab" data-bs-target="#materiales"
-                                type="button" role="tab" aria-controls="materiales" aria-selected="true">Materiales de
-                                construcción</a>
+                                type="button" role="tab" aria-controls="materiales" aria-selected="true">Hogar</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" id="electricos-tab" data-bs-toggle="tab" data-bs-target="#electricos"
@@ -18,16 +17,18 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" id="maderas-tab" data-bs-toggle="tab" data-bs-target="#maderas"
-                                type="button" role="tab" aria-controls="maderas" aria-selected="true">Maderas</a>
+                                type="button" role="tab" aria-controls="maderas" aria-selected="true">Herramientas</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="dropdown-categorias" data-bs-toggle="dropdown"
+                            <!-- <a class="nav-link dropdown-toggle" id="dropdown-categorias" data-bs-toggle="dropdown"
                                 href="#" role="button" aria-expanded="false">Más categorías</a>
                             <ul class="dropdown-menu" aria-labelledby="dropdown-categorias">
                                 <li><a class="dropdown-item" href="#">Opción 1</a></li>
                                 <li><a class="dropdown-item" href="#">Opción 2</a></li>
                                 <li><a class="dropdown-item" href="#">Opción 3</a></li>
-                            </ul>
+                            </ul> -->
+                            <a class="nav-link" id="otros-tab" data-bs-toggle="tab" data-bs-target="#otros"
+                                type="button" role="tab" aria-controls="otros" aria-selected="true">Otros</a>
                         </li>
                         <li class="nav-item">
                             <form class="d-flex justify-content-end mx-auto">
@@ -57,13 +58,13 @@
                 </div>
                 <div class="tab-pane fade" id="materiales" role="tabpanel" aria-labelledby="materiales-tab">
                     <section class="row">
-                        <div v-for= "item in productosArray" :key= "item.id" class="col-12 col-md-4 col-lg-3 mt-5">
-                            <div class="card m-auto shadow" style="width: 200px;">
-                                <img v-bind:src= "item.img" class="card-img-top" alt="Imagen producto">
+                        <div v-for= "item in forHogar" :key= "item.id"  class="col-12 col-md-4 col-lg-3 mt-5">
+                            <div  class="card m-auto shadow" style="width: 200px;">
+                                <img v-bind:src= "item.url" class="card-img-top" alt="Imagen producto">
                                 <div class="card-body border-top text-start">
-                                    <h5 class="card-title color-marca">"Segunda" + {{item.nombreProducto}}</h5>
+                                    <h5 class="card-title color-marca">{{item.nombre}}</h5>
                                     <p class="card-text text-black">
-                                        {{item.descripcionProducto}}
+                                        {{item.descripcion}}
                                     </p>
                                     <h5 class="card-title text-black">{{item.precio}}</h5>
                                 </div>
@@ -73,13 +74,13 @@
                 </div>
                 <div class="tab-pane fade" id="electricos" role="tabpanel" aria-labelledby="electricos-tab">
                     <section class="row">
-                        <div v-for= "item in productosArray" :key= "item.id1" class="col-12 col-md-4 col-lg-3 mt-5">
+                        <div v-for= "item in forElectricos" :key= "item.id1" class="col-12 col-md-4 col-lg-3 mt-5">
                             <div class="card m-auto shadow" style="width: 200px;">
-                                <img v-bind:src="item.img" class="card-img-top" alt="Imagen producto">
+                                <img v-bind:src= "item.url" class="card-img-top" alt="Imagen producto">
                                 <div class="card-body border-top text-start">
-                                    <h5 class="card-title color-marca">"Tercera " + {{item.nombreProducto}}</h5>
+                                    <h5 class="card-title color-marca">{{item.nombre}}</h5>
                                     <p class="card-text text-black">
-                                        {{item.descripcionProducto}}
+                                        {{item.descripcion}}
                                     </p>
                                     <h5 class="card-title text-black">{{item.precio}}</h5>
                                 </div>
@@ -89,13 +90,29 @@
                 </div>
                 <div class="tab-pane fade" id="maderas" role="tabpanel" aria-labelledby="maderas-tab">
                     <section class="row">
-                        <div v-for= "item in productosArray" :key= "item.id3" class="col-12 col-md-4 col-lg-3 mt-5">
+                        <div v-for= "item in forHerramientas" :key= "item.id3" class="col-12 col-md-4 col-lg-3 mt-5">
                             <div class="card m-auto shadow" style="width: 200px;">
-                                <img v-bind:src="item.img" class="card-img-top" alt="Imagen producto">
+                                <img v-bind:src= "item.url" class="card-img-top" alt="Imagen producto">
                                 <div class="card-body border-top text-start">
-                                    <h5 class="card-title color-marca">"Cuarta " + {{item.nombreProducto}}</h5>
+                                    <h5 class="card-title color-marca">{{item.nombre}}</h5>
                                     <p class="card-text text-black">
-                                        {{item.descripcionProducto}}
+                                        {{item.descripcion}}
+                                    </p>
+                                    <h5 class="card-title text-black">{{item.precio}}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div class="tab-pane fade" id="otros" role="tabpanel" aria-labelledby="otros-tab">
+                    <section class="row">
+                        <div v-for= "item in forOtros" :key= "item.id3" class="col-12 col-md-4 col-lg-3 mt-5">
+                            <div class="card m-auto shadow" style="width: 200px;">
+                                <img v-bind:src= "item.url" class="card-img-top" alt="Imagen producto">
+                                <div class="card-body border-top text-start">
+                                    <h5 class="card-title color-marca">{{item.nombre}}</h5>
+                                    <p class="card-text text-black">
+                                        {{item.descripcion}}
                                     </p>
                                     <h5 class="card-title text-black">{{item.precio}}</h5>
                                 </div>
@@ -112,38 +129,6 @@
         data() {
             return {
                 articulosTabla: [],
-                productosArray : [
-                {
-                    img: require('../assets/producto.png'),
-                    nombreProducto:"Taladro percutor",
-                    descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
-                    precio: "$ 200.000"
-                },
-                {
-                    img: require('../assets/producto.png'),
-                    nombreProducto:"Taladro percutor",
-                    descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
-                    precio: "$ 200.000"
-                },
-                {
-                    img: require('../assets/producto.png'),
-                    nombreProducto:"Taladro percutor",
-                    descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
-                    precio: "$ 200.000"
-                },
-                {
-                    img: require('../assets/producto.png'),
-                    nombreProducto:"Taladro percutor",
-                    descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
-                    precio: "$ 200.000"
-                },
-                {
-                    img: require('../assets/producto.png'),
-                    nombreProducto:"Taladro percutor",
-                    descripcionProducto: "Taladro Percutor 1/2 pul 650w 3150RPM 47250GPM",
-                    precio: "$ 200.000"
-                }     
-                ],
             }
         },
         created(){
@@ -160,6 +145,21 @@
                     console.log(e.response);
                 })
             } 
+        },
+        computed:{
+            forHogar: function(){
+                return this.articulosTabla.filter(i => i.categoria === 'Hogar')
+            },
+            forElectricos: function(){
+                return this.articulosTabla.filter(i => i.categoria === 'Electricos')
+            },
+            forHerramientas: function(){
+                return this.articulosTabla.filter(i => i.categoria === 'Herramientas')
+            },
+            forOtros: function(){
+                return this.articulosTabla.filter(i => i.categoria === 'Otros')
+            }
+            
         }
     }
 </script>
