2756d9d4d5b5731e4a2051dabe15e4a225812c64
Yixsi <yixsi3103@gmail.com>
2021-11-10 21:22:36 -0500

    Update nosotros

--- frontend/src/components/nosotros.vue
+++ frontend/src/components/nosotros.vue
@@ -1,53 +1,55 @@
 
 <template>
         <div class="container mx-auto mt-5 mb-5">
-            <section class="row mb-5 .bg-dark">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-
-                            <video width="1000" height="250" autoplay muted loop id="myVideo">
-                                <source src="../assets/video.mp4" type="video/mp4">
-                                <p>Su navegador no soporta video HTML5. Aquí hay un <a href="rabbit320.mp4">enlace al video</a>.</p>
-                            </video>
+            <section class="tarjeta row mb-5 .bg-dark col-lg-4 col-md-6 col-sm-12">
+                    <div class="wrapper mx-auto">
+                        <div class="card front"><img src="https://images.squarespace-cdn.com/content/v1/53c4566ae4b047b01385cdb6/1542663621032-QSYRS85LCRJXVIGWYBVY/julio_ferreterias_mar23.jpg?format=1000w"/>
+                            <div class="info">
+                            <h1>¿Quiénes somos?</h1>
+                            <p>Somos una empresa dedicada a la distribución de productos eléctricos y de ferretería a los diferentes municipios del Valle del Cauca</p>
+                            <button class="btn-card" data-bs-toggle="modal" data-bs-target="#about">Leer más</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-
-                <section class="row d-flex justify-content-evenly text-black ">   
-                        <div class="col-10 col-md-5 my-auto card text-start" >
-                            <div class="card-body">
-                                <h1 class="color-marca card-title">¿Quiénes somos?</h1>
-                                <p class="lead card-text">
-                                    Somos una empresa dedicada a la distribución de productos eléctricos y de ferretería a los diferentes municipios 
-                                    del Valle del Cauca, mediante el innovador y eficiente servicio; generando progreso y estabilidad a nuestros 
-                                    colaboradores, ya que ellos hacen posible la existencia de nuestra empresa y  siendo así un aliado estratégico de 
-                                    nuestros clientes a la hora de remodelar sus hogares.
-                                </p>
-                                <p class="lead ">
-                                    Nuestra compañía inicia un proceso comercial en la ciudad de candelaria, donde se vio  un gran potencial  por su 
-                                    expansión gracias los nuevos proyectos de vivienda, actualmente buscamos  ser los mejores aliados de los propietarios 
-                                    de las viviendas y ser partícipes en hacer cumplir los sueños de los clientes ofreciendo una gran variedad de productos 
-                                    para la remodelación de sus hogares.
-                                </p>
+                    <div class="modal fade" tabIndex="-1" id="about" aria-hidden="true" aria-labelledby="about">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content text-white" v-bind:style="{ backgroundImage: 'url(' + require('../assets/6882.jpg')+')' }">
+                                    <div class="modal-body">
+                                        <button type="button" class="close btn-close" data-bs-dismiss="modal" aria-label="Close" style="background-color: white">
+                                        </button>
+                                        <p class="lead-about">
+                                        Somos una empresa dedicada a la distribución de productos eléctricos y de ferretería a diferentes municipios 
+                                        del Valle del Cauca, brindando un servicio innovador y eficiente. 
+                                        <br/>Generamos progreso y estabilidad en nuestros 
+                                        colaboradores, ya que ellos hacen posible la existencia de nuestra empresa.
+                                        </p>
+                                        <p class="lead-about">
+                                        Nuestra compañía inicia un proceso comercial en la ciudad de Candelaria, donde se vio un gran potencial de expansión gracias los nuevos proyectos de vivienda; por consiguiente, buscamos ser los mejores aliados de los residentes y ser partícipes en hacer cumplir sus sueños, ofreciendo una gran variedad de productos para la remodelación de sus hogares.
+                                        </p> 
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-10 col-md-5 my-auto text-start card">
-                            <div class="card-body">
-                                <h2 class="color-marca card-title">Misión</h2>
-                                <p class="lead">
-                                    Ofrecer productos de calidad a nuestros clientes, contribuyendo a la mejora de sus viviendas y cumpliendo sus 
-                                    sueños de remodelar su hogar.
-                                </p>
-                                <h2 class="color-marca card-title">Visión</h2>
-                                <p class="lead">
-                                    Ser reconocidos como una empresa responsable con sus clientes, que brinda buen servicio y ofrece productos de 
-                                    calidad a nuestros clientes. Ser el mejor aliado del ferretero al proveer materiales de construcción y 
-                                    productos de ferretería.
-                                </p>
+                </section>
+                <section class="tarjeta row mb-5 .bg-dark col-lg-4 col-md-6 col-sm-12">
+                    <div class="wrapper mx-auto">
+                        <div class="card"><img src="https://img.freepik.com/foto-gratis/pareja-joven-haciendo-reparacion-apartamentos-juntos-ellos-mismos-hombre-mujer-casados-haciendo-remodelacion-o-renovacion-casa-concepto-relaciones-familia-amor-midiendo-pared-preparandose-diseno_155003-19797.jpg?size=626&ext=jpg"/>
+                            <div class="info">
+                            <h1>Misión</h1>
+                            <p>Ofrecer productos de calidad a nuestros clientes, contribuyendo a la mejora de sus viviendas y cumpliendo sus sueños de remodelar su hogar.</p>
                             </div>
                         </div>
+                    </div>
+                </section>
+                <section class="tarjeta row mb-5 .bg-dark col-lg-4 col-md-6 col-sm-12">
+                    <div class="wrapper mx-auto">
+                        <div class="card"><img src="https://img.freepik.com/foto-gratis/constructor-masculino-probandose-casco-estante-ferreteria-constructor-uniforme-mirar-productos-tienda-bricolaje_266732-20737.jpg?size=626&ext=jpg"/>
+                            <div class="info">
+                            <h1>Visión</h1>
+                            <p>Ser reconocidos como una empresa responsable con sus clientes, que brinda buen servicio y ofrece productos de calidad a nuestros clientes. Ser el mejor aliado del ferretero al proveer materiales de construcción y productos de ferretería.</p>
+                            </div>
+                        </div>
+                    </div>
                 </section>
 
         </div>
